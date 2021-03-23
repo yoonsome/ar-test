6eadde4f9380c5c32df1315d4399cf81da901131
hollymollyguakamolly <julie5173@naver.com>
2021-03-23 22:46:34 +0900

    [ADD]uaengArr

--- script.js
+++ script.js
@@ -42,12 +42,21 @@ var setModel = function (model, entity) {
     entity.setAttribute('gltf-model', model.url);
 
     const div = document.querySelector('.instructions');
-    // div.innerText = model.info;
 };
+
+let usaengArr = [["A1", "교육학과"], ["A2", "수학교육과"], ["A3", "컴퓨터교육과"],
+["A4", "한문교육과"], ["A5", "소비자가족학과"], ["A6", "아동청소년학과"],
+["B1", "경영학과"], ["C1", "러시아어문학과"], ["C2", "한문학과"], ["C3", "철학과"],
+["C4", "중어중문학과"], ["C5", "불어불문학과"], ["C6", "국어국문학과"], ["C7", "사학과"],
+["C8", "문헌정보학과"], ["C9", "경제학과"], ["D1", "미술학과"], ["D2", "영상학과"],
+["D3", "연기예술학과"], ["D4", "의상학과"], ["D5", "무용학과"], ["D6", "디자인학과"],
+["E1", "글로벌리더학과"], ["F1", "글로벌경영"], ["F2", "글로벌경제"]]
+
 
 // 팝업 js
 function onClick(){
     document.querySelector(".container").classList.add('modal-open');
+
 }
 
 function onClose(){
